85c2868b5774454fa3ba68da16a710c2c1f13c6c
Eduardo Ribeiro <eduardoribeirocury@gmail.com>
2021-01-10 12:12:13 -0400

    Criando componente de exception

---
+++ src/app/shared/components/components.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ExceptionComponent } from './exception/exception.component';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    ExceptionComponent,
+  ]
+})
+export class ComponentsModule { }
