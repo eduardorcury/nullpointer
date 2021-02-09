9ba97572c64844206c776d631c89ff5f2f1c146e
Eduardo Ribeiro <eduardoribeirocury@gmail.com>
2021-02-09 17:39:28 -0300

    Adicionando markdown it

--- src/app/pages/new/new.component.ts
+++ src/app/pages/new/new.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 
+declare var require: any;
+
 @Component({
   selector: 'app-new',
   templateUrl: './new.component.html',
@@ -8,16 +10,34 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class NewComponent {
 
+  markdown: any;
+  solutionHtml: string;
+
   form = this.formBuilder.group({
-    title: ['', Validators.required, Validators.minLength(5)],
-    description: ['', Validators.required, Validators.minLength(5)],
-    solution: ['', Validators.required, Validators.minLength(5)]
+    title: ['', [Validators.required, Validators.minLength(5)]],
+    description: ['', [Validators.required, Validators.minLength(5)]],
+    solution: ['', [Validators.required, Validators.minLength(5)]]
   });
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder) {
 
-  onSubmit() {
+    this.markdown = require('markdown-it')({
+      html: true,
+      linkify: true,
+      typographer: true
+    });
+    this.onChanges();
+
+  }
+
+  onSubmit(): void {
     console.log(this.form.value);
+  }
+
+  onChanges(): void {
+    this.form.get('solution').valueChanges.subscribe(value => {
+      this.solutionHtml = this.markdown.render(value);
+    })
   }
 
 }
